69fc95d946e8c3b21ceef4abdc2fe9f7e6105ba1
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-05-21 06:00:29 +0300

    Add SizeConfig

--- lib/core/utils/helper.dart
+++ lib/core/utils/helper.dart
@@ -1,18 +1,69 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Helper {
-  static void setSystemUIOverlayStyle({
-    required Color systemNavigationBarColor,
-    required Color statusBarColor,
-    required Brightness statusBarBrightness,
-  }) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        systemNavigationBarColor: systemNavigationBarColor,
-        statusBarColor: statusBarColor,
-        statusBarBrightness: statusBarBrightness,
+import '../global/app_colors.dart';
+
+void setSystemUIOverlayStyle({
+  Color systemNavigationBarColor = AppColors.scaffoldBackgroundColor,
+  Color statusBarColor = AppColors.scaffoldBackgroundColor,
+  Brightness statusBarBrightness = Brightness.dark,
+}) {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: systemNavigationBarColor,
+      statusBarColor: statusBarColor,
+      statusBarBrightness: statusBarBrightness,
+    ),
+  );
+}
+
+enum SnackBarStates { success, error, warning }
+
+// The User's Id
+String? uId;
+
+void buildSnackBar({
+  required String message,
+  required SnackBarStates state,
+  required BuildContext context,
+}) {
+  final SnackBar snackBar = SnackBar(
+    content: Text(
+      message,
+      style: const TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
       ),
-    );
+      textAlign: TextAlign.center,
+    ),
+    dismissDirection: DismissDirection.down,
+    clipBehavior: Clip.hardEdge,
+    backgroundColor: chooseSnackBarClr(state),
+    duration: const Duration(seconds: 2),
+    behavior: SnackBarBehavior.floating,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15.0),
+    ),
+    padding: const EdgeInsets.all(15.0),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+//================== For Easy Choosing The Color Of The SnackBar ==================
+Color chooseSnackBarClr(SnackBarStates state) {
+  Color color;
+  switch (state) {
+    case SnackBarStates.success:
+      color = Colors.green;
+      break;
+    case SnackBarStates.error:
+      color = Colors.red;
+      break;
+    case SnackBarStates.warning:
+      color = Colors.amber;
+      break;
   }
+  return color;
 }
